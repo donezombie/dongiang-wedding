ef4e5981e2c3e4fdfe7c83797b74248212bc9737
donezombie <donezombie@gmail.com>
2022-12-11 02:10:53 +0700

    Add parallax scroll

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import Demo1 from 'assets/demo1.webp';
 
 import 'animate.css';
 import './scss/styles.scss';
+import { Parallax } from 'react-scroll-parallax';
 
 const App = () => {
+  //! State
+
   //! Render
   return (
     <div className='main-page'>
@@ -14,11 +17,12 @@ const App = () => {
         <div className='main-slider__bg-img' style={{ backgroundImage: `url(${Demo1})` }} />
 
         <div className='main-slider__text'>
-          <img className="animate__animated animate__fadeIn" alt="flower" src={FlowerImg} />
-          <p className="animate__animated animate__fadeIn">Đôn & Giang</p>
-          <p className="animate__animated animate__fadeIn">WEDDING PHOTOGRAPHY</p>
+          <Parallax translateY={[-60, 80]}>
+            <img className="animate__animated animate__fadeIn" alt="flower" src={FlowerImg} />
+            <p className="animate__animated animate__fadeIn">Đôn & Giang</p>
+            <p className="animate__animated animate__fadeIn">WEDDING PHOTOGRAPHY</p>
+          </Parallax>
         </div>
-
       </section>
     </div>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -4,38 +4,19 @@ import { I18nextProvider } from 'react-i18next';
 
 import './index.css';
 import App from './App';
-import store from './redux/store';
 import * as serviceWorker from './serviceWorker';
-import i18n from 'i18n';
 import { createRoot } from 'react-dom/client';
-import AuthProvider from 'providers/AuthenticationProvider';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import reportWebVitals from 'reportWebVitals';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <Provider store={store}>
-        <AuthProvider>
-          <App />
-          <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-          />
-        </AuthProvider>
-      </Provider>
-    </I18nextProvider>
+    <ParallaxProvider>
+      <App />
+    </ParallaxProvider>
   </React.StrictMode>,
 );
 
